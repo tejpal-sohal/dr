ed8da421a12e268af4df65740968e8fc30cd4d68
tejpal-sohal <tejpal54@gmail.com>
2020-06-22 13:06:36 +0100

    All

---
+++ precache-manifest.53c5b2bd7a7c162e16d2143ff41fd5a7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f2964026f566a08f617cf6ca9a9d4ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d94eb80a808aaf1feccf",
+    "url": "/static/css/main.a832fce6.chunk.css"
+  },
+  {
+    "revision": "d7f8cadff924eccc08f8",
+    "url": "/static/js/2.cdf16928.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cdf16928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d94eb80a808aaf1feccf",
+    "url": "/static/js/main.b022ddb5.chunk.js"
+  },
+  {
+    "revision": "55c1d1486efbab4838c6",
+    "url": "/static/js/runtime-main.2df90ec7.js"
+  },
+  {
+    "revision": "ead3550adfee1a025c138f9478f42ab3",
+    "url": "/static/media/blur-bg.ead3550a.jpg"
+  },
+  {
+    "revision": "3a4d4c648a79ecabbc52b84493c083da",
+    "url": "/static/media/rocket.3a4d4c64.png"
+  }
+]);
